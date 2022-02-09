4cd07972552885417e3965b4317be4cb05530db4
DaseiN-461 <88171023+DaseiN-461@users.noreply.github.com>
2022-02-08 21:07:43 -0600

    Update spending_machine_v1.c

--- spending_machine_v1.c
+++ spending_machine_v1.c
@@ -8,7 +8,7 @@ enum{
     verificate,
     give_rest,
 };
-int process_state = wait_money;
+int process_state = verificate;
 
 /*types of errors */
 enum{
@@ -77,6 +77,14 @@ products basket_user = {
     .product_C_num = 1,
 };
 
+/* el comprador luego de seleccionar lo que desea indica que la compra
+esta lista para ser procesada */
+bool basket_ready(){
+    return 1;
+}
+
+
+
 /* this function will activate when the user select a product,
  and verificate if the money is enough */
 bool money_enough(){
@@ -91,15 +99,29 @@ bool money_enough(){
     }
 }
 
-/* this function will disconunt money that take for the basket */
+/* this function will verificate that coins in the
+machine is enough */
+bool coins_enough(){
+    return 1;
+}
 
-void discount_coins_user(coins *coins_user_ptr, coins *coins_machine_ptr){
+
+/* this function will verificate that the products selected
+ is available */
+bool products_available(){
+    return 1;
+}
+
+
+/* this function will disconunt money that take for the basket */
+void trade_coins_to_buy(coins *coins_user_ptr, coins *coins_machine_ptr){
     int money_of_user = wallet_calc(&coins_user);
     int basket_price = basket_calc(&basket_user, &products_in_the_machine);
-    int money_to_discount = basket_price;
-    int money_user_post_buy = money_of_user - money_to_discount;
-    printf("money_discount: %d, money_post: %d\n\r", money_to_discount, money_user_post_buy);
 
+    int rest = money_of_user - basket_price;
+    printf("rest: %d\n\r", rest);
+
+    /* define the coins of user and machine */
     int coins_50_user = coins_user_ptr->coins_50;
     int coins_10_user = coins_user_ptr->coins_10;
     int coins_5_user = coins_user_ptr->coins_5;
@@ -110,57 +132,98 @@ void discount_coins_user(coins *coins_user_ptr, coins *coins_machine_ptr){
     int coins_5_machine = coins_machine_ptr->coins_5;
     int coins_1_machine = coins_machine_ptr->coins_1;
 
-    while (money_to_discount > 0)
-    {
-        while (money_to_discount - 50 >= 0)
-        {
-            money_to_discount = money_to_discount - 50;
-            coins_50_user = coins_50_user - 1;
-            coins_50_machine = coins_50_machine + 1;
+    /* print the coins of user */
+    printf("user coins ----50: %d, 10: %d, 5: %d, 1: %d\n\r", coins_user_ptr->coins_50,coins_user_ptr->coins_10,coins_user_ptr->coins_5,coins_user_ptr->coins_1 );
+
+    /* machine get all coins and after give the rest */
+    coins_50_machine = coins_50_machine + coins_50_user;
+    coins_10_machine = coins_10_machine + coins_10_user;
+    coins_5_machine = coins_5_machine + coins_5_user;
+    coins_1_machine = coins_1_machine + coins_1_user;
+
+    /* delete the coins register of the user */
+    coins_50_user = 0;
+    coins_10_user = 0;
+    coins_5_user = 0;
+    coins_1_user = 0;
+
+    /* give the rest of the buy */
+
+    while (rest > 0){
+        while(rest >= 50){
+            coins_50_user = coins_50_user + 1;
+            coins_50_machine = coins_50_machine - 1;
+            rest = rest - 50;
         }
-        while (money_to_discount - 10 >= 0)
-        {
-            money_to_discount = money_to_discount - 10;
-            coins_10_user = coins_10_user - 1;
-            coins_10_machine = coins_10_machine + 1;        
+        while(rest >= 10){
+            coins_10_user = coins_10_user + 1;
+            coins_10_machine = coins_10_machine - 1;
+            rest = rest - 10;
         }
-        while (money_to_discount - 5 >= 0)
-        {
-            money_to_discount = money_to_discount - 5;
-            coins_5_user = coins_5_user - 1;
-            coins_5_machine = coins_5_machine + 1;
+        while(rest >= 5){
+            coins_5_user = coins_5_user + 1;
+            coins_5_machine = coins_5_machine - 1;
+            rest = rest - 5;
         }
-        while (money_to_discount - 1 >= 0)
-        {
-            money_to_discount = money_to_discount - 1;
-            coins_1_user = coins_1_user - 1;
-            coins_1_machine = coins_1_machine + 1;
+        while(rest >= 1){
+            coins_1_user = coins_1_user + 1;
+            coins_1_machine = coins_1_machine - 1;
+            rest = rest - 1;
         }
     }
-        
-    coins_user_ptr->coins_50 = coins_50_user;
-    coins_user_ptr->coins_10 = coins_50_user;
-    coins_user_ptr->coins_5 = coins_50_user;
-    coins_user_ptr->coins_1 = coins_50_user;
 
+    /* save the new coins of the machine */
     coins_machine_ptr->coins_50 = coins_50_machine;
     coins_machine_ptr->coins_10 = coins_10_machine;
     coins_machine_ptr->coins_5 = coins_5_machine;
     coins_machine_ptr->coins_1 = coins_1_machine;
-        
+
+    coins_user_ptr->coins_50 = coins_50_user;
+    coins_user_ptr->coins_10 = coins_10_user;
+    coins_user_ptr->coins_5 = coins_5_user;
+    coins_user_ptr->coins_1 = coins_1_user;
+
+    printf("user coins ----50: %d, 10: %d, 5: %d, 1: %d\n\r", coins_user_ptr->coins_50,coins_user_ptr->coins_10,coins_user_ptr->coins_5,coins_user_ptr->coins_1 );
 }
 
-/* this function will verificate that coins in the
-machine is enough */
-bool coins_enough(){
-    return 1;
+
+
+
+void discount_product_of_machine(products *ptr_basket, products *ptr_machine){
+    /* se puede optimizar con algún ciclo for */
+    int product_A_basket = ptr_basket->product_A_num;
+    int product_B_basket = ptr_basket->product_B_num;
+    int product_C_basket = ptr_basket->product_C_num;
+
+    int product_A_machine = ptr_machine->product_A_num;
+    int product_B_machine = ptr_machine->product_B_num;
+    int product_C_machine = ptr_machine->product_C_num;
+
+    product_A_machine = product_A_machine - product_A_basket;
+    product_B_machine = product_B_machine - product_B_basket;
+    product_C_machine = product_C_machine - product_C_basket;
+
+    printf("p_A: %d, p_B: %d, p_C: %d\n\r", product_A_machine, product_B_machine, product_C_machine);
+
+    ptr_machine->product_A_num = product_A_machine;
+    ptr_machine->product_B_num = product_B_machine;
+    ptr_machine->product_C_num = product_C_machine;
 }
 
-/* this function will verificate that the products selected
- is available */
-bool products_available(){
-    return 1;
+void delete_basket(products *ptr_basket){
+    ptr_basket->product_A_num = 0;
+    ptr_basket->product_B_num = 0;
+    ptr_basket->product_C_num = 0;
+    printf("user -- pA: %d, pB: %d, pC: %d\n\r",ptr_basket->product_A_num, ptr_basket->product_B_num,ptr_basket->product_C_num);
 }
+
+/* entregar las monedas físicamente y eliminar registros de estas
+   para esperar una siguiente compra */
+void give_rest_coins(){
+    return;
+}
+
+
 
 /* this function calculate the money necessary to buy the basket of the user */
 int basket_calc(products *basket_products_ptr, products *price_ptr){
@@ -192,8 +255,14 @@ void handle_machine(){
     switch (process_state)
     {
     case wait_money:
-        if (money_enough()){
+        /* recibir las monedas y esperar a que el carro sea el deseado */
+        if(basket_ready()){
             process_state = verificate;
+        }
+        break;
+    case verificate:
+        if (money_enough()){
+            process_state = give_rest;
         }
         else{
             error_state = money_insufficient;
@@ -203,32 +272,32 @@ void handle_machine(){
         }
         break;
 
-    case verificate:
+    case give_rest:
             /* coins enough to give rest */
             if(coins_enough() && products_available()){
-                /* descontar dinero de la wallet del usuario y calcular el vuelto */
-                /* crear funcion */
-                discount_coins_user(&coins_user, &coin_in_the_machine);
+                /* take the user coins and give the rest */
+                trade_coins_to_buy(&coins_user, &coin_in_the_machine);
+                discount_product_of_machine(&basket_user, &products_in_the_machine);
+                delete_basket(&basket_user);
                 printf("the purchase is done succesfully\n\r");
-                process_state = give_rest;
+                give_rest_coins();
+                /* carro vacío y monedas entregadas, ya esta
+                listo para la proxima compra */
+                process_state = wait_money;
             }
             else{
                 if (!coins_enough()){
                     error_state = no_coins;
                     printf("the machine no have sufficient money to give the rest\n");
+                    return;
                 }
                 if (!products_available()){
                     error_state = no_product;
                     printf("the machine no have the product\n");
+                    return;
                 }
             }
         break;
-    
-    case give_rest:
-        printf("new record");
-        return;
-        break;
-
     default:
         break;
     }
@@ -240,8 +309,10 @@ void main(){
     print_wallets();
     handle_machine();
     handle_machine();
-    print_wallets();
+    handle_machine();
 
     
     return 0;
 }
+
+
