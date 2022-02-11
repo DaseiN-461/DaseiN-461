a64494e878314e35c69d7b1ac2a96f721a2b9a62
DaseiN-461 <88171023+DaseiN-461@users.noreply.github.com>
2022-02-11 00:21:09 -0600

    Add files via upload

---
+++ wireless_spending_machine_v2/html.h
@@ -0,0 +1,28 @@
+String pagina = "<!DOCTYPE html>"
+"<html>"
+"<head>"
+"<meta charset='utf-8' />"
+"<title>Spending machine web</title>"
+"</head>"
+"<body>"
+"<center>"
+"<h1>Spending machine web</h1>"
+
+/*Coins buttons */
+"<h2>Press the buttons to insert coins</h2>"
+"<p><a href='/A'><button style='height:50px;width:100px'>1 coin</button></a></p>"
+"<p><a href='/B'><button style='height:50px;width:100px'>5 coins</button></a></p>"
+"<p><a href='/C'><button style='height:50px;width:100px'>10 coins</button></a></p>"
+
+"<h2>Press the buttons to select the product</h2>"
+"<p><a href='/D'><button style='height:50px;width:100px'>Product A</button></a></p>"
+
+"<p><a href='/E'><button style='height:50px;width:100px'>Buy</button></a></p>"
+
+
+
+
+
+"</center>"
+"</body>"
+"</html>";
